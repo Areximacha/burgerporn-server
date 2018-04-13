a2b76cf6679194470c55db800724d258c599c2e8
Jay Tang <jay.tang@arcadiagroup.co.uk>
2018-04-13 11:12:54 +0100

    slight refactor for cleanliness

--- app/routes/test_routes.js
+++ app/routes/test_routes.js
@@ -4,57 +4,56 @@ const router = express.Router()
 const Note = require('../models/note')
 
 module.exports = (path) => {
-  router.post(`${path}`, (req, res) => {
-    const note = new Note()
-    note.title = req.body.title
-    note.text = req.body.body
+  router.route(`${path}`)
+    .post((req, res) => {
+      const note = new Note()
+      note.title = req.body.title
+      note.text = req.body.body
 
-    note.save((err) => {
-      if (err)
-        res.send(err)
+      note.save((err) => {
+        if (err)
+          res.send(err)
 
-      res.json({ message: 'Note created!' })
+        res.json({ message: 'Note created!' })
+      })
     })
-  })
+    .get((req, res) => {
+      Note.find((err, notes) => {
+        if (err)
+          res.send(err)
 
-  router.get(`${path}`, (req, res) => {
-    Note.find((err, notes) => {
-      if (err)
-        res.send(err)
-
-      res.json(notes)
+        res.json(notes)
+      })
     })
-  })
 
-  router.get(`${path}/:id`, (req, res) => {
-    Note.findById(req.params.id, (err, note) => {
-      if (err)
-        res.send(err)
-      
-      res.json(note)
+  router.route(`${path}/:id`)
+    .get((req, res) => {
+      Note.findById(req.params.id, (err, note) => {
+        if (err)
+          res.send(err)
+        
+        res.json(note)
+      })
     })
-  })
+    .put((req, res) => {
+      // shouldn't need to recast the body keys and db schema if done properly
+      const note = { text: req.body.body, title: req.body.title }
 
-  router.put(`${path}/:id`, (req, res) => {
-    // shouldn't need to recast the body keys and db schema if done properly
-    const note = { text: req.body.body, title: req.body.title }
+      Note.findByIdAndUpdate(req.params.id, note, (err, response) => {
+        if (err)
+          res.send(err)
 
-    Note.findByIdAndUpdate(req.params.id, note, (err, response) => {
-      if (err)
-        res.send(err)
-
-      res.json({ message: 'Note updated!' })
+        res.json({ message: 'Note updated!' })
+      })
     })
-  })
+    .delete((req, res) => {
+      Note.findByIdAndRemove(req.params.id, (err, response) => {
+        if (err)
+          res.send(err)
 
-  router.delete(`${path}/:id`, (req, res) => {
-    Note.findByIdAndRemove(req.params.id, (err, response) => {
-      if (err)
-        res.send(err)
-
-      res.json({ message: 'Note deleted!' })
+        res.json({ message: 'Note deleted!' })
+      })
     })
-  })
 
   return router
 }
