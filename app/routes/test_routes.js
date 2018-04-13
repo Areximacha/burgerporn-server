7dc031cc17613fa80cc9e81e192baf078e4574e2
Jay Tang <jay.tang@arcadiagroup.co.uk>
2018-04-13 11:07:33 +0100

    adding all test CRUD routes

--- app/routes/test_routes.js
+++ app/routes/test_routes.js
@@ -15,7 +15,45 @@ module.exports = (path) => {
 
       res.json({ message: 'Note created!' })
     })
+  })
 
+  router.get(`${path}`, (req, res) => {
+    Note.find((err, notes) => {
+      if (err)
+        res.send(err)
+
+      res.json(notes)
+    })
+  })
+
+  router.get(`${path}/:id`, (req, res) => {
+    Note.findById(req.params.id, (err, note) => {
+      if (err)
+        res.send(err)
+      
+      res.json(note)
+    })
+  })
+
+  router.put(`${path}/:id`, (req, res) => {
+    // shouldn't need to recast the body keys and db schema if done properly
+    const note = { text: req.body.body, title: req.body.title }
+
+    Note.findByIdAndUpdate(req.params.id, note, (err, response) => {
+      if (err)
+        res.send(err)
+
+      res.json({ message: 'Note updated!' })
+    })
+  })
+
+  router.delete(`${path}/:id`, (req, res) => {
+    Note.findByIdAndRemove(req.params.id, (err, response) => {
+      if (err)
+        res.send(err)
+
+      res.json({ message: 'Note deleted!' })
+    })
   })
 
   return router
